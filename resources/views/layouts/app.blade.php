71ae147880d84b10f8348757f754e58bef1eb90a
Irene Brignoli <ire.brignoli@gmail.com>
2023-05-10 15:33:53 +0200

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!--font-->
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+  <!-- Styles -->
+  @vite('resources/js/app.js')
+
+  <title>@yeld('page-title')</title>
+</head>
+<body>
+  
+</body>
+</html>
